19abddd4a860d10aa09c3d3b61ec6170b7f40a71
TunrayoIlawole <tunrayoilawole99@gmail.com>
2021-07-03 05:30:34 +0100

    use http module to fetch data

---
+++ src/app/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string = '../assets/album.json'
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl)
+    .pipe(
+      map(Response => {
+        return Response.json();
+      })
+    )
+  }
+
+}
