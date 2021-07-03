6aa4870c5401fe365e6527cef822d633139d41e3
TunrayoIlawole <tunrayoilawole99@gmail.com>
2021-07-03 05:38:55 +0100

    update product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,11 +11,7 @@ export class ProductService {
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
-    .pipe(
-      map(Response => {
-        return Response.json();
-      })
-    )
+      .map((response) => response.json());
   }
 
 }
